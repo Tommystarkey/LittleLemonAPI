a1e3e6f89862978c8f307dd05ace2cb473db70de
Tommy Starkey <delstarkeydelstarkey@gmail.com>
2023-11-16 11:04:42 +0000

    adding another model serializer

--- djangowork/coursera/LittleLemon/LittleLemon/LittleLemonAPI/serializers.py
+++ djangowork/coursera/LittleLemon/LittleLemon/LittleLemonAPI/serializers.py
@@ -1,17 +1,23 @@
 from rest_framework import serializers #required to use serializers functions
 # from .models import MenuItem #imports the model
-from .models import EmployeeList
+from .models import EmployeeList, MenuItem
+from decimal import Decimal
 
-# class MenuItemSerializer(serializers.ModelSerializer): #by inheriting from serializer class i get a lot of serialization logic for free, automaticaly generates serializers from underlying model
-#     class Meta: #meta class is used to specify meta data about serialization proccess
-#         model = MenuItem #specifys which model to base serializer on
-#         fields = ["id","title", 'price', 'inventory'] #specifies which fields to include
+class MenuItemSerializer(serializers.ModelSerializer): #by inheriting from serializer class i get a lot of serialization logic for free, automaticaly generates serializers from underlying model
+    stock = serializers.IntegerField(source='inventory') #creates now field and links it to an existing field with the scource argument
+    price_after_tax = serializers.SerializerMethodField(method_name= 'calculate_tax')
+    class Meta: #meta class is used to specify meta data about serialization proccess
+        model = MenuItem #specifys which model to base serializer on
+        fields = ["id","title", 'price', 'stock', 'price_after_tax'] #specifies which fields to include
+    
+    def calculate_tax(self, product:MenuItem):
+        return product.price * Decimal(1.1)
 
-class MenuItemSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2)
-    inventory = serializers.IntegerField()
+# class MenuItemSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=255)
+#     price = serializers.DecimalField(max_digits=6, decimal_places=2)
+#     inventory = serializers.IntegerField()
 
 class EmployeeListSerializer(serializers.ModelSerializer):#'Modelserializer is a class that automatically generates serializers so you have to write less code
     class Meta:#specifies the metadata about the serialization progress
